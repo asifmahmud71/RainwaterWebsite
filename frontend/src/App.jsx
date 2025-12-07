572979c712917fbf7cc6ddfc41bf9781635522c3
asifmahmud71 <asifmahmudmeraj2016@gmail.com>
2025-12-08 03:54:10 +0600

    faqs testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import RegistrationPage from './components/RegistrationPage';
 import AdminPage from './components/AdminPage';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home');
+  const [currentPage, setCurrentPage] = useState('faqs');
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
